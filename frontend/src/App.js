c2b31a6328efee38c0f77fb856a12bc12df99343
Manudevice <manudevice@gmail.com>
2025-05-08 23:04:24 +0000

    nuevos cambios

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import TreeComponent from "./TreeComponent";
+
+function App() {
+  return (
+    <div style={{ width: "100%", height: "500px" }}>
+      <TreeComponent data={treeData} />
+    </div>
+  );
+}
